70faede64b658840e04d5863ddc0c945b49cb392
Silja <69155440+siljabl@users.noreply.github.com>
2022-02-13 15:53:43 +0100

    Update main_typeII.cpp

--- code/main_typeII.cpp
+++ code/main_typeII.cpp
@@ -1,4 +1,4 @@
-#include "evolution/distributions.cpp"
+#include "evolution/parameters.cpp"
 #include "evolution/food_web.cpp"
 #include "evolution/time_series_typeII.cpp"
 #include "evolution/stability_analysis.cpp"
